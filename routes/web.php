5ae490f94f5baeeabe426795c0f33d6d239c9cbd
Anatoly <chistowick@yandex.ru>
2020-06-24 21:57:35 +0300

    Добавил маршрут для основной страницы и маршрут для обработки post-запроса AJAX (получение первоначального списка карточек)

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return view('main');
 });
+
+Route::post('ajax/get-cards', 'AjaxController@getCards');
