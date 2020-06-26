140a800dcc4588b2529bcaaf97fbf080fdf049a5
Anatoly <chistowick@yandex.ru>
2020-06-26 20:59:09 +0300

    В контроллере для обработки AJAX запросов создал метод для добавления новой карточки в БД и возврата ID добавленной карточки клиентскому скрипту

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -9,26 +9,46 @@ class AjaxController extends Controller {
 
     /**
      * 
-     * @return type $cards - array;
+     * @return void;
      * 
-     * Returns array of cards.
+     * Printss array of cards - JSON.
      */
     public function getCards() {
 
         $cards = Card::select
-                (
-                'id',
-                'original',
-                'originalComment',
-                'translation',
-                'translationComment',
-                'stack'
+                        (
+                        'id', 'original', 'originalComment', 'translation', 'translationComment', 'stack'
                 )
                 ->where('login', 'chistowick')
                 ->get();
-        
+
         // Converts the array to JSON and returns it to js-client
         echo json_encode($cards);
+
+        return;
+    }
+
+    /**
+     * 
+     * @return void;
+     * 
+     * Saves a new card. Prints record success status.
+     */
+    public function addCard(Request $request) {
+
+        $newCard = Card::create(
+                        [
+                            'original' => $request->original,
+                            'originalComment' => $request->originalComment,
+                            'translation' => $request->translation,
+                            'translationComment' => $request->translationComment,
+                            'stack' => $request->stack,
+                        ]
+        );
+
+        echo json_encode($newCard->id);
+
+        return;
     }
 
 }
