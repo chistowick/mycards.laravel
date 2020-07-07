c50cc2c63e063260465f2ccec07bfb6973b9d276
Anatoly <chistowick@yandex.ru>
2020-07-07 20:40:46 +0300

    Добавил в AjaxController два метода: 1) Метод для редактирования карточки в базе согласно данным от js клиента. 2) Метод для удаления карточки из базы по id от js клиента

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request; // для работы с Request
 use App\Card; // Использовать пространство имен модели Book
 
-class AjaxController extends Controller {
+class AjaxController extends Controller
+{
 
     /**
      * 
@@ -13,14 +14,19 @@ class AjaxController extends Controller {
      * 
      * Printss array of cards - JSON.
      */
-    public function getCards() {
+    public function getCards()
+    {
 
-        $cards = Card::select
-                        (
-                        'id', 'original', 'originalComment', 'translation', 'translationComment', 'stack'
-                )
-                ->where('login', 'chistowick')
-                ->get();
+        $cards = Card::select(
+            'id',
+            'original',
+            'originalComment',
+            'translation',
+            'translationComment',
+            'stack'
+        )
+            ->where('login', 'chistowick')
+            ->get();
 
         // Converts the array to JSON and returns it to js-client
         echo json_encode($cards);
@@ -32,18 +38,19 @@ class AjaxController extends Controller {
      * 
      * @return void;
      * 
-     * Saves a new card. Prints record success status.
+     * Saves a new card. Prints id of new card.
      */
-    public function addCard(Request $request) {
+    public function addCard(Request $request)
+    {
 
         $newCard = Card::create(
-                        [
-                            'original' => $request->original,
-                            'originalComment' => $request->originalComment,
-                            'translation' => $request->translation,
-                            'translationComment' => $request->translationComment,
-                            'stack' => $request->stack,
-                        ]
+            [
+                'original'              => $request->original,
+                'originalComment'       => $request->originalComment,
+                'translation'           => $request->translation,
+                'translationComment'    => $request->translationComment,
+                'stack'                 => $request->stack,
+            ]
         );
 
         echo json_encode($newCard->id);
@@ -51,4 +58,59 @@ class AjaxController extends Controller {
         return;
     }
 
+    /**
+     *
+     * Edits the card. Prints status.
+     *
+     * @return void;
+     * 
+     **/
+    public function editCard(Request $request)
+    {
+        $editedCardId = $request->id;
+
+        // TODO Нужно добавить проверку логина из сессии, чтобы пользователь мог редактировать только свои карточки
+
+        $editedCard = Card::find($editedCardId); //Получаем модель карточки, которую хотим отредактировать
+
+        // Изменяем карточку
+        $editedCard->original           = $request->original;
+        $editedCard->originalComment    = $request->originalComment;
+        $editedCard->translation        = $request->translation;
+        $editedCard->translationComment = $request->translationComment;
+        $editedCard->stack              = $request->stack;
+
+        // Сохраняем изменения в базу и возвращаем статус операции клиенту JS
+        // TODO Проверить тип данных, в котором TRUE/FALSE придет в JS
+        if ($editedCard->save()) {
+            echo json_encode([TRUE]);
+        } else {
+            echo json_encode([FALSE]);
+        }
+
+        return;
+    }
+
+    /**
+     *
+     * Deletes the card by id. Prints status.
+     *
+     * @return void;
+     * 
+     **/
+    public function deleteCard(Request $request)
+    {
+        $deletedCardId = $request->id;
+
+        // TODO Нужно добавить проверку логина из сессии, чтобы пользователь мог редактировать только свои карточки
+
+        // Удаляем модель по id, переданному от JS клиента и возвращаем статус операции
+        if (Card::destroy($deletedCardId)) {
+            echo json_encode([TRUE]);
+        } else {
+            echo json_encode([FALSE]);
+        }
+
+        return;
+    }
 }
