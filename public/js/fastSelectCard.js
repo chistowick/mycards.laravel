0e9f95bef64a32eabf0a9ed28496ca41a84daaee
Anatoly <chistowick@yandex.ru>
2020-07-16 17:53:04 +0300

    Добавил сортировку options каждого selecta по алфавиту с сохранением возможности быстрого перехода к карточке. Также добавил возможность смены редактируемой карточки через SELECT

--- public/js/fastSelectCard.js
+++ public/js/fastSelectCard.js
@@ -26,17 +26,20 @@ $(document).ready(function () {
         // Обновляем отображение активной карточки
         updateCardView();
 
-        // Отменяем selected в обоих SELECT чтобы не путать пользователя
-        // $(`#selectElementOriginal_${curentNumberActiveCard}`).prop('selected', false);
-        // $(`#selectElementTranslation_${curentNumberActiveCard}`).prop('selected', false);
+        // Если активен режим редактирования
+        if (addOrEdit === 'edit') {
+            // Заполняем поля формы данными из активной карточки
+            fillEditForm();
+        }
 
+        // Задаем selected нулевому элементу в обоих SELECT чтобы не путать пользователя
         $('select').prop('selectedIndex', 0);
 
     });
 });
 
 // Функция, которая формирует выпадающий список элемента FASTSELECT
-function fastSelectlistCreate() {
+function fastSelectlistCreate(callback = selectSort) {
 
     // Если массив с активными карточками уже существует
     if (activeCardsArray) {
@@ -85,6 +88,37 @@ function fastSelectlistCreate() {
             fastSelectTranslation.lastChild.setAttribute('value', i);
         });
 
+        // Сортируем по алфавиту
+        callback(fastSelectOriginal);
+        callback(fastSelectTranslation);
     }
 
+}
+
+function selectSort(select) {
+    
+    for (let i = 1; i < select.children.length; i++){
+        for (let j = (i+1); j < select.children.length; j++){
+            if (select.children[i].text.toLowerCase() > select.children[j].text.toLowerCase()){
+
+                // replacedNode = parentNode.replaceChild(newChild, oldChild);
+
+                let replacedNode = select.replaceChild(select.children[j], select.children[i]); // временно стертый option i (больший элемент)
+
+                // Вставить временно удаленный option сразу после обновленного
+                insertAfter(replacedNode, select.children[i]);
+                
+            }
+        }
+    }
+
+}
+
+// Вставить newElement сразу перед referenceElement в группе дочерних элементов родителя referenceElement
+function insertAfter(newElement, referenceElement) {
+    
+    // var insertedElement = parentElement.insertBefore(newElement, referenceElement);
+
+    // Вставляем временно стертый пункт сразу перед последующим элементом (т.е. после обновленного)
+    return referenceElement.parentNode.insertBefore(newElement, referenceElement.nextSibling);
 }
